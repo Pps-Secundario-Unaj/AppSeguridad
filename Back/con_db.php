ff8bbb80d2d03e61c66e26b3a72f390041eb6106
Pps-Secunario-Unaj <ppssecunarios.unaj@gmail.com>
2024-11-14 14:53:09 -0300

    camb jonatan

--- Back/con_db.php
+++ Back/con_db.php
@@ -1,3 +1,3 @@
 <?php
 
-$conex = mysqli_connect("localhost", "root", "", "alarma"); //cambios para nuestra app
+$conex = mysqli_connect("localhost", "root", "", "alarma"); //cambios para nuestra app jonatan
